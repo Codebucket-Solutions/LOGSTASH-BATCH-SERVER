2cd7557beaf723fbeed187fcb965859d91de3439
Abhinav Gautam <abhinav.nanhey@gmail.com>
2024-04-08 18:52:03 +0530

    new line

--- src/utils/queue-batch-processor.ts
+++ src/utils/queue-batch-processor.ts
@@ -3,13 +3,15 @@ import Logstash from "./logstash";
 
 export default async (queue: Queue, logstash: Logstash) => {
   const jobs = await queue.getJobs(undefined, 0, -1, true);
-  let jobData = [];
+  let jobData = "";
   for (let job of jobs) {
-    jobData.push(job.data);
+    jobData = jobData + JSON.stringify(job.data) + "\r\n";
   }
-  logstash.log(JSON.stringify(jobData), async () => {
-    for (let job of jobs) {
-      await job.remove();
-    }
-  });
+  if (jobData.length > 0) {
+    logstash.log(jobData, async () => {
+      for (let job of jobs) {
+        await job.remove();
+      }
+    });
+  }
 };
